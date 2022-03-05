51caedcef162b0d151820b8e5ce02b6e4c704736
guhaapratim <65932947+guhaapratim@users.noreply.github.com>
2022-03-05 12:53:28 +0530

    Add files via upload

---
+++ WinWebServer.ps1
@@ -0,0 +1,21 @@
+﻿Configuration WindowsWebServer
+{
+  Node 'localhost'
+      {
+        # Install IIS - Enabled via Windows Feature
+        WindowsFeature IIS
+         {
+            Ensure = "Present"
+            Name = "Web-Server"
+            IncludeAllSubFeature = $true	
+          }
+        # Create the DefaultDocument
+        File DefaultDocument
+         {
+	    Ensure = "Present"
+            Type = "File"
+	    DestinationPath = "C:\Inetpub\wwwroot\index.html"
+	    Contents = "Hello World !! from Windows Web Server"
+          }
+      }
+} 
